a3ef8834dbebc218b89b6845457a33554a5c75c4
Sylvia Karani <sylviagkarani@gmail.com>
2025-02-05 19:48:49 +0300

    Deploy docassemble with terraform and github actions

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "eu-central-1"
+}
+
+resource "aws_lightsail_instance" "docassemble"{
+    name                = "docassemble"
+    availability_zone   = "eu-central-1a"
+    blueprint_id        = "ubuntu_22_04"
+    bundle_id           = "nano_2_0"
+    key_pair_name       = "new-key-pair"
+}
+
+output "instance_ip" {
+    value = aws_lightsail_instance.docassemble.public_ip_address
+}
