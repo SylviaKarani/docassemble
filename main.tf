95a89f1e54ba4b819f63b2cd04981a0bb1ee00af
Sylvia Karani <sylviagkarani@gmail.com>
2025-02-05 22:44:53 +0300

    changes on ssh agent and key

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "eu-central-1"
 }
 
-resource "aws_lightsail_instance" "terraform-2"{
-    name                = "terraform-2"
+resource "aws_lightsail_instance" "orange"{
+    name                = "orange"
     availability_zone   = "eu-central-1a"
     blueprint_id        = "ubuntu_22_04"
     bundle_id           = "nano_2_0"
@@ -11,5 +11,5 @@ resource "aws_lightsail_instance" "terraform-2"{
 }
 
 output "instance_ip" {
-    value = aws_lightsail_instance.terraform-2.public_ip_address
+    value = aws_lightsail_instance.orange.public_ip_address
 }
