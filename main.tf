d6e35104762155e4d604f14a5242566022ca7504
Sylvia Karani <sylviagkarani@gmail.com>
2025-02-05 21:40:16 +0300

    changes on docker installation and restarting

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "eu-central-1"
 }
 
-resource "aws_lightsail_instance" "docassemble-deployment"{
-    name                = "docassemble-deployment"
+resource "aws_lightsail_instance" "deployment"{
+    name                = "deployment"
     availability_zone   = "eu-central-1a"
     blueprint_id        = "ubuntu_22_04"
     bundle_id           = "nano_2_0"
@@ -11,5 +11,5 @@ resource "aws_lightsail_instance" "docassemble-deployment"{
 }
 
 output "instance_ip" {
-    value = aws_lightsail_instance.docassemble-deployment.public_ip_address
+    value = aws_lightsail_instance.deployment.public_ip_address
 }
