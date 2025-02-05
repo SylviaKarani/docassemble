1863e65aab1a8c8d750da032efb9e37f6b3d7b19
Sylvia Karani <sylviagkarani@gmail.com>
2025-02-05 21:08:32 +0300

    changes on hcl file

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "eu-central-1"
 }
 
-resource "aws_lightsail_instance" "docassemble"{
-    name                = "docassemble"
+resource "aws_lightsail_instance" "docassemble-terraform"{
+    name                = "docassemble-terraform",
     availability_zone   = "eu-central-1a"
     blueprint_id        = "ubuntu_22_04"
     bundle_id           = "nano_2_0"
