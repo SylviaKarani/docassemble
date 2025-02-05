66d19a72f8c732c477500b1d27a5e47cb35477f7
Sylvia Karani <sylviagkarani@gmail.com>
2025-02-05 21:21:39 +0300

    changes on instance name

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "eu-central-1"
 }
 
-resource "aws_lightsail_instance" "docassemble-terraform"{
-    name                = "docassemble-terraform"
+resource "aws_lightsail_instance" "docassemble-deploy"{
+    name                = "docassemble-deploy"
     availability_zone   = "eu-central-1a"
     blueprint_id        = "ubuntu_22_04"
     bundle_id           = "nano_2_0"
@@ -11,5 +11,5 @@ resource "aws_lightsail_instance" "docassemble-terraform"{
 }
 
 output "instance_ip" {
-    value = aws_lightsail_instance.docassemble-terraform.public_ip_address
+    value = aws_lightsail_instance.docassemble-deploy.public_ip_address
 }
