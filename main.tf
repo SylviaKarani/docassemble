3d305372fb0bfc7b809f71ddb6be42ef9fcc5767
Sylvia Karani <sylviagkarani@gmail.com>
2025-02-05 21:52:52 +0300

    changes on github actions on lightsail

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "eu-central-1"
 }
 
-resource "aws_lightsail_instance" "deployment-instance"{
-    name                = "deployment-instance"
+resource "aws_lightsail_instance" "deployment-terraform"{
+    name                = "deployment-terraform"
     availability_zone   = "eu-central-1a"
     blueprint_id        = "ubuntu_22_04"
     bundle_id           = "nano_2_0"
@@ -11,5 +11,5 @@ resource "aws_lightsail_instance" "deployment-instance"{
 }
 
 output "instance_ip" {
-    value = aws_lightsail_instance.deployment-instance.public_ip_address
+    value = aws_lightsail_instance.deployment-terraform.public_ip_address
 }
