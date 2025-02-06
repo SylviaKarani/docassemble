1c8ae39310be1eda5d562a7ca194630c4c7ebd77
Sylvia Karani <sylviagkarani@gmail.com>
2025-02-06 17:28:07 +0300

    fixes

--- main.tf
+++ main.tf
@@ -1,15 +1,29 @@
 provider "aws" {
-    region = "eu-central-1"
+  region = "eu-central-1"
 }
 
-resource "aws_lightsail_instance" "orange"{
-    name                = "orange"
-    availability_zone   = "eu-central-1a"
-    blueprint_id        = "ubuntu_22_04"
-    bundle_id           = "nano_2_0"
-    key_pair_name       = "new-key-pair"
+variable "instance_name" {
+  description = "Name for the instance"
+  default     = "docassemble-instance"
+}
+
+resource "aws_lightsail_key_pair" "keypair" {
+  name       = "lightsail_key"
+  public_key = file("/home/jinks/lightsail_key.pub")
+}
+
+resource "aws_lightsail_instance" "docassemble" {
+  name              = var.instance_name
+  availability_zone = "eu-central-1a"
+  blueprint_id      = "ubuntu_22_04"
+  bundle_id         = "nano_2_0"
+  key_pair_name     = aws_lightsail_key_pair.keypair.name
+
+  tags = {
+    Name = var.instance_name
+  }
 }
 
 output "instance_ip" {
-    value = aws_lightsail_instance.orange.public_ip_address
+  value = aws_lightsail_instance.docassemble.public_ip_address
 }
