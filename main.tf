7e8b6dce825e6647084c1b688e43a93ec30e494d
Sylvia Karani <sylviagkarani@gmail.com>
2025-02-06 21:35:43 +0300

    private key

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ variable "instance_name" {
 }
 
 resource "aws_lightsail_key_pair" "my-key-pair" {
-  name       = "light-sail-key-v3"
+  name       = "light-sail-key-v4"
   public_key = file("lightsail_key.pub")
 }
 
-resource "aws_lightsail_instance" "docassemblesEtup" {
+resource "aws_lightsail_instance" "DocassemblesEtup" {
   name              = var.instance_name
   availability_zone = "eu-central-1a"
   blueprint_id      = "ubuntu_22_04"
@@ -25,5 +25,5 @@ resource "aws_lightsail_instance" "docassemblesEtup" {
 }
 
 output "instance_ip" {
-  value = aws_lightsail_instance.docassemblesEtup.public_ip_address
+  value = aws_lightsail_instance.DocassemblesEtup.public_ip_address
 }
