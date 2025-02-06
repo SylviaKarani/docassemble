b357ce32b5795bba7aa689156d3fe326801a2f57
Sylvia Karani <sylviagkarani@gmail.com>
2025-02-06 20:08:18 +0300

    instance_ip

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ variable "instance_name" {
 }
 
 resource "aws_lightsail_key_pair" "my-key-pair" {
-  name       = "light-sail-key"
+  name       = "light-sail-key-v1"
   public_key = file("lightsail_key.pub")
 }
 
-resource "aws_lightsail_instance" "docassemble-setup" {
+resource "aws_lightsail_instance" "docassemble_setup" {
   name              = var.instance_name
   availability_zone = "eu-central-1a"
   blueprint_id      = "ubuntu_22_04"
@@ -25,5 +25,5 @@ resource "aws_lightsail_instance" "docassemble-setup" {
 }
 
 output "instance_ip" {
-  value = aws_lightsail_instance.docassemble-setup.public_ip_address
+  value = aws_lightsail_instance.docassemble_setup.public_ip_address
 }
